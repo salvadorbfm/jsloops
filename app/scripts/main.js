9814240abdb2734d7be1910314c1bb8c99486cda
Salvador Elizarrarás <salvadorbfm@gmail.com>
2016-06-30 03:30:11 -0500

    Removed commented code in main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,3 @@
-// var cm = CodeMirror($('#editor')[0], {
-//   value: '// jsLoops - 2015 ',
-//   theme: '3024-night',
-//   mode:  'javascript',
-//   lineNumbers: true,
-//   tabSize: 2
-// });
-//
-// var context = new AudioContext();
-// var Engine = new JSLoopsEngine(context);
-//
-// cm.on('keydown', function(_cm, e) {
-//   var now = context.currentTime;
-//   console.log(e.metaKey, e.keyCode);
-//   if (e.metaKey && e.keyCode === 13) {
-//     Engine.DrumKick.trigger(now + 0.5);
-//     Engine.DrumKick.trigger(now + 1.5);
-//     Engine.DrumKick.trigger(now + 2.5);
-//   }
-// });
-
+// JSLoops @SalvadorBFM
 var context = new AudioContext(),
     handler = new JSLoopsHandler(context);
